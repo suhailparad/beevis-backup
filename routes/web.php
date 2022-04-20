588b2fead284afd33e695b9612b3a6e5e09a11b9
suhailparad <pk.suhail47@gmail.com>
2022-04-20 12:42:36 +0530

    Breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\MigrationController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/migrate/user', [MigrationController::class,'migrateUser']);
-Route::get('/migrate/order', [MigrationController::class,'migrateOrder']);
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/migrate-tax-rates', [MigrationController::class,'migrateTaxRate']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
